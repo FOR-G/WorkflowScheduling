55a95b49683e4e13e637aebbdbe7be22eaaa4d92
Nuoye <18844186785@163.com>
2019-11-24 17:14:02 +0800

    balaba

--- TestData/data.py
+++ TestData/data.py
@@ -1,6 +1,10 @@
 import random
+import matplotlib.pyplot as plt
+from matplotlib import patches
+import numpy as np
+from matplotlib.collections import PatchCollection
 
-from model import Location,User,Server
+from model import Location, User, Server
 
 '''
 先假设一些数据
@@ -8,7 +12,60 @@ from model import Location,User,Server
 服务器 半径20
 
 '''
-locList = []
-for i in range(1,11):
-    loc = Location(random.uniform(-100.0, 100.0,2),random.uniform(-100.0, 100.0,2))
-    loc.print()
+
+# locList = []
+
+
+# 产生随机位置
+def getRandomLocation():
+    x1 = random.uniform(-50.0, 50.0)
+    x2 = random.uniform(-50.0, 50.0)
+    # loc = Location(x1,x2)
+    loc = Location(format(x1, '.2f'), format(x2, '.2f'))
+    return loc
+
+
+# 得到N个位置
+def getNPos(N):
+    locLi = []
+    for i in range(N):
+        locLi.append(getRandomLocation())
+    return locLi
+
+
+# 存入N个服务器的数据
+def saveServer(N):
+    server = []
+    for i in range(N):
+        ser = Server(random.randint(20, 25), random.randint(10, 25),getRandomLocation().latitude,getRandomLocation().longitude, )
+        server.append(ser)
+    with open("Server_data.txt", 'w',encoding='utf8') as file:
+        for s in server:
+            file.write(str(s)+'/n')
+
+# saveServer(10)
+# 存入N个用户/工作流
+
+
+
+
+
+# 画出服务器的为位置
+def plotCircle(Li= []):
+    patch = []
+    x = np.linspace(-100, 100, 20)
+    y = np.linspace(-100, 100, 20)
+    fig, ax = plt.subplots(figsize=(-50, 50), dpi=10)
+    for i in Li:
+        circle = patches.Circle((i.latitude, i.longitude), 20, color="#BBFFFF", alpha=0.2)
+        patch.append(circle)
+    collection = PatchCollection(patch)
+    ax.add_collection(collection)
+    plt.show()
+
+#
+# for i in range(1, 11):
+#     locList.append(getRandomLocation())
+# plotCircle(locList)
+
+
--- model.py
+++ model.py
@@ -11,24 +11,37 @@ class Location:
         self.latitude = lati
         self.longitude = longi
 
+    def __str__(self):
+        return str(self.latitude)+' '+str(self.longitude)
+
     def print(self):
-        print('[' + str(self.latitude) + ' ' + str(self.longitude) + ']')
+        print('[' + str(self) + ']')
 
 
 class User(Location):
     # workflow 只考虑顺序结构 列表表示
-    def __init__(self, lati, longi, workf):
+    def __init__(self, lati, longi, workf=[]):
         super(User, self).__init__(lati, longi)
         self.workflow = workf
+
+    def __str__(self):
+        return str(self.latitude)+' '+str(self.longitude)+' '+str(self.workflow)
+
+    def print(self):
+        print('['+str(self)+']')
 
 
 class Server(Location):
     # 服务器包含位置 价格 范围
-    def __init__(self, radius, cost, lati=0, longi=0):
+    def __init__(self, radius, cost, lati, longi):
         super(Server, self).__init__(lati, longi)
-        self.radius = radius
+        self.ra = radius
         self.cost = cost
 
+    def __str__(self):
+        return str(self.latitude)+' '+str(self.longitude)+' '+str(self.ra)+' '+str(self.cost)
 
+    def print(self):
+        print('[' + str(self) + ']')
 '''编码'''
 # 任务编码，
